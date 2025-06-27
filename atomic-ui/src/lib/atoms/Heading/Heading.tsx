4dd10d32e45c337f74cdb4719c2bfb256bdd55f9
faiza.nb <faizanb.mec@gmail.com>
2025-06-27 22:55:55 +0530

    heading atom added

---
+++ atomic-ui/src/lib/atoms/Heading/Heading.tsx
@@ -0,0 +1,14 @@
+import { JSX } from 'react';
+import styles from './Heading.module.scss';
+
+type HeadingType = 'H1' | 'H2' | 'H3' | 'H4' | 'H5' | 'H6';
+interface HeadingProps {
+  text: string;
+  type: HeadingType;
+}
+
+export const Heading = ({ text, type }: HeadingProps) => {
+  const HeadingTag = type.toLowerCase() as keyof JSX.IntrinsicElements;
+
+  return <HeadingTag className={styles.heading}>{text}</HeadingTag>;
+};
