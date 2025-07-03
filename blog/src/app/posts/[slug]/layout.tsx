0197f7f310a3f9247afb5ddc4b8285ed1efab9ef
faiza.nb <faizanb.mec@gmail.com>
2025-07-03 20:40:07 +0530

    theme provided added to switch themes

--- blog/src/app/posts/[slug]/layout.tsx
+++ blog/src/app/posts/[slug]/layout.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { Nunito_Sans } from 'next/font/google';
 import styles from './layout.module.scss';
-
-const layout_font = Nunito_Sans({
-  weight: ['200', '300', '400', '500', '700'],
-  style: 'normal',
-  subsets: ['latin'],
-  display: 'swap',
-});
 
 export default function PostLayout({
   children,
@@ -16,7 +8,7 @@ export default function PostLayout({
   children: React.ReactNode;
 }) {
   return (
-    <body className={`${layout_font.className} ${styles.body}`}>
+    <>
       <header className={styles.header}>
         <div className={styles.blogLogoContainer}>
           <Link href="/" className="port-link">
@@ -29,10 +21,10 @@ export default function PostLayout({
             <span className={styles.heading}>faizanb.dev</span>
           </Link>
         </div>
-        <span>{` > `}</span>
+        <span>{` | `}</span>
         <span className={styles.placeholder}>Blog</span>
       </header>
       {children}
-    </body>
+    </>
   );
 }
