b684f6b60d5ad8775b1c5a13557dde02896472a7
faiza.nb <faizanb.mec@gmail.com>
2025-07-08 22:39:59 +0530

    toc molecule added

---
+++ atomic-ui/src/lib/molecules/TableOfContent/TableOfContent.tsx
@@ -0,0 +1,28 @@
+import styles from './TableOfContent.module.scss';
+
+type TocItem = {
+  id: number;
+  text: string;
+  level: string;
+  anchor: string;
+};
+
+const TableOfContent = ({ toc }: { toc: TocItem[] }) => (
+  <nav className={styles.tocNav} aria-label="Table of Contents">
+    <h2 className={styles.title}>Table of Contents</h2>
+    <ul className={styles.tocList}>
+      {toc.map((item) => (
+        <li
+          key={item.id}
+          className={styles[`level${item.level}`] || styles.tocItem}
+        >
+          <a href={`#${item.anchor}`} className={styles.tocLink}>
+            {item.text}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+
+export default TableOfContent;
