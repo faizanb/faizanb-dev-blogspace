5b74656b29b5ac0a0a94dd14eb1acf61f1cc3f63
faiza.nb <faizanb.mec@gmail.com>
2025-07-08 22:40:36 +0530

    export added for toc

--- atomic-ui/src/index.ts
+++ atomic-ui/src/index.ts
@@ -4,4 +4,6 @@ export { default as Heading } from './lib/atoms/Heading/Heading';
 export { default as DateLabel } from './lib/atoms/DateLabel/DateLabel';
 export { default as Paragraph } from './lib/atoms/Paragraph/Paragraph';
 
+export { default as TableOfContent } from './lib/molecules/TableOfContent/TableOfContent';
+
 export { default as CodeBlock } from './lib/organisms/CodeBlock/CodeBlock';
