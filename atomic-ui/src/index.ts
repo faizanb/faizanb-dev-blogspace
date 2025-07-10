1a690d20157b97ef23e46ac0497c764689a8ad2f
faiza.nb <faizanb.mec@gmail.com>
2025-07-10 22:42:58 +0530

    export added

--- atomic-ui/src/index.ts
+++ atomic-ui/src/index.ts
@@ -3,6 +3,7 @@ export { default as PageTitle } from './lib/atoms/PageTitle/PageTitle';
 export { default as Heading } from './lib/atoms/Heading/Heading';
 export { default as DateLabel } from './lib/atoms/DateLabel/DateLabel';
 export { default as Paragraph } from './lib/atoms/Paragraph/Paragraph';
+export { default as FeaturedImage } from './lib/atoms/FeaturedImage/FeaturedImage';
 
 export { default as TableOfContent } from './lib/molecules/TableOfContent/TableOfContent';
 
